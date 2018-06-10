2a86312c347739195fcdf1ee9a5f344fc843273f
Ravichander <RaviChanderDev1@gmail.com>
2018-06-10 15:52:07 +0530

    Ravi Change1

--- SampleApp/src/com/rcr/test/MainClass.java
+++ SampleApp/src/com/rcr/test/MainClass.java
@@ -3,7 +3,9 @@ package com.rcr.test;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		for(int i=1;i<=100;i++){
+			System.out.println(i);
+		}
 	}
 
 }
